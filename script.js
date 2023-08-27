d5c371c0c830ce2774823bf6258e2016264143a8
Brian <brianessayan@hotmail.com>
2023-08-27 11:17:54 +0200

    I have added the function in charge of requesting a value to the user.

--- script.js
+++ script.js
@@ -35,3 +35,18 @@ function playRound(playerSelection, computerSelection) {
     }
   }
 
+// getPlayerChoice(): prompts the user for a value.
+function getPlayerChoice() {
+    let validatedInput = false;
+    while (validatedInput == false) {
+      const choice = prompt("Rock Paper Scissors");
+      if (choice == null) {
+        continue;
+      }
+      const choiceInLower = choice.toLowerCase();
+      if (options.includes(choiceInLower)) {
+        validatedInput = true;
+        return choiceInLower;
+      }
+    }
+  }
